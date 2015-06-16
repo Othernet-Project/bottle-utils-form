3c48b94f97de0bc926d868bf00284f734fae56d5
Andrean Franc <andrean.franc@gmail.com>
2015-06-16 15:17:00 +0200

    Fix ValidationError constructor so is_form param is actualy used

--- bottle_utils/form.py
+++ bottle_utils/form.py
@@ -17,7 +17,7 @@ class ValidationError(Exception):
     def __init__(self, message, params, is_form=False):
         self.message = message
         self.params = params
-        self.is_form = False
+        self.is_form = is_form
         super(ValidationError, self).__init__(message)
 
     def __str__(self):
